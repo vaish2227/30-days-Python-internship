f6bb2b9756a1d4e7eb6b0069222e02d4e473c343
vaish2227 <85924040+vaish2227@users.noreply.github.com>
2021-06-15 21:57:28 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,22 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=40
+>>> d=a/10
+>>> m=b*50
+>>> add=c+60
+>>> print(d,m,add)
+4.0 2000 100
+>>> str1="Vaishnavi"
+>>> str_var="pooja"
+>>> print(str_var.replace("o","G"))
+pGGja
+>>> str_var="vaish"
+>>> print(str_var.replace("i","G"))
+SyntaxError: multiple statements found while compiling a single statement
+>>> a=20
+>>> b=20.5
+>>> print("After typecasting ",float(a))
+After typecasting  20.0
+>>> print("After typecasting",int(b))
+After typecasting 20
+>>> 
