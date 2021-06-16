18baa6dc39f0b2e49d110783616445c3509576ba
vaish2227 <85924040+vaish2227@users.noreply.github.com>
2021-06-16 21:20:50 +0530

    Add files via upload

---
+++ Day 5(exercise 2&3).py
@@ -0,0 +1,11 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_tuple=(22,33,44,55,66,77)
+>>> temp=reversed(my_tuple)
+>>> print(tuple(temp))
+(77, 66, 55, 44, 33, 22)
+>>> tuple2=(2,4,6,8,10)
+>>> temp2=list(tuple2)
+>>> print(temp2)
+[2, 4, 6, 8, 10]
+>>> 
